beb6a308090304d9f31c4f3b9d3194cb7cba7bea
Ninoshka Perez <perez.ninoshka31@gmail.com>
2024-01-19 12:52:32 -0400

    fix answer fail

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,12 +8,14 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+	size_t j;
+	int temp, swap;
 
-	for (i = 0; i < size - 1; i++)
+	while (swap != 0)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		swap = 0;
+
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -21,6 +23,7 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swap = 1;
 
 				/* Print after swap */
 				print_array(array, size);
